4fbf5e835e91c23e008b06516a5393e27ac0839f
user <email@gmail.com>
2025-11-14 09:04:47 +0200

    strada

---
+++ app.js
@@ -0,0 +1,19 @@
+const numberInput = document.getElementById("guess");
+const button = document.getElementById("enter");
+const output = document.getElementById("output");
+let min = 1;
+let max = 100;
+const secretNumber = Math.floor(Math.random() * (max - min + 1));
+
+button.addEventListener("click", guessingTheNumber);
+
+function guessingTheNumber() {
+    const guess = Number(numberInput.value);
+    if (guess < secretNumber) {
+        output.innerHTML = "Number too low. Try again";
+    } else if (guess > secretNumber) {
+        output.innerHTML = "Number too high. Try again";
+    } else {
+        output.innerHTML = "Congratulations, you guessed the number!";
+    }
+}
