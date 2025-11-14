e637c0a2732fbdd8a9225072f46219c76d204bea
user <email@gmail.com>
2025-11-14 09:36:28 +0200

    strada

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ let min = 1;
 let max = 100;
 const secretNumber = Math.floor(Math.random() * (max - min + 1));
 
-button.addEventListener("click", guessingTheNumber);
-
+button.addEventListener("click", guessingTheNumber,);
 function guessingTheNumber() {
     const guess = Number(numberInput.value);
+    guess.count
     if (guess < secretNumber) {
         output.innerHTML = "Number too low. Try again";
     } else if (guess > secretNumber) {
         output.innerHTML = "Number too high. Try again";
     } else {
-        output.innerHTML = "Congratulations, you guessed the number!";
+        console.log(guess)
+        output.innerHTML = "Congratulations, you guessed the number! You took: ???? Guesses";
     }
 }
